13201eaed0e7331e6eaa82854fca5e18f730bcd7
Andrea Conti <contiandrea96@gmail.com>
2019-03-20 07:57:57 +0100

    Haddock Search.hs

--- hsearch/src/Search.hs
+++ hsearch/src/Search.hs
@@ -35,6 +35,8 @@ import           Control.Applicative
 import           Data.Maybe (maybeToList, listToMaybe)
 
 -- TYPES --
+
+-- | max depth to search for
 data MaxDepth = Forever | Until Int deriving (Eq, Show)
 
 -- | Goal utility in order to find first goal in a list o nodes
@@ -44,7 +46,8 @@ findGoal f = listToMaybe . take 1 . filter f
 
 -- GENERIC SEARCH --
 
--- | 'searchUntilDepth' function provides a high level customizable way to specific a search algorithm
+-- | /searchUntilDepth/ provides a high level interface for a generic Search Tree algorithm with custom max
+--   depth to search for
 searchUntilDepth :: (Eq s, Ord i) => MaxDepth          -- ^ max depth to search
                                   -> (SNode s -> i)    -- ^ policy to be used
                                   -> CheckTime         -- ^ when apply check goal
@@ -68,7 +71,8 @@ searchUntilDepth !md p !ct cg g s = join . maybeToList $ loop initFrontier
                 Nothing   -> loop (insert cFr $ map (RSNode rsNode) newSNodes)
                 Just goal -> return $! map SN.state . RN.rsNodeToList $ rsNode
 
--- | 'iterativeSearch' implements a search using a progressive depth until result is found
+-- | /iterativeSearch/ is equivalent to @searchUntilDepth@ called on increasing depths until the goal is found. Used with
+--   a depth first like policy can provide features similar to a search in amplitude (breadth first like policy)
 iterativeSearch :: (Eq s, Ord i) => (SNode s -> i)    -- ^ policy to be used
                                  -> CheckTime         -- ^ when apply check goal
                                  -> (s -> Bool)       -- ^ function used to check if the goal is reached
@@ -82,7 +86,8 @@ iterativeSearch p ct cg g s = let depths = [1..]
                                   find (x:_)   = x
                               in  find results
 
--- | 'search' function provides a high level customizable way to specific a search algorithm
+-- | /searchUntilDepth/ provides a high level interface for a generic Search Tree algorithm which search until
+--   the goal is not found
 search :: (Eq s, Ord i) => (SNode s -> i)    -- ^ policy to be used
                         -> CheckTime         -- ^ when apply check goal
                         -> (s -> Bool)       -- ^ function used to check if the goal is reached
@@ -93,28 +98,37 @@ search = searchUntilDepth Forever
 
 -- NOT INFORMED SEARCH --
 
--- | 'breadth-first search' implementation
+-- | search with breadth first policy. The root node is expanded first, then all its childrens and so
+--   on, It's /complete/ and /optimal/ but very expensive in time and space complexity (/O(b^d)/ with
+--   b branching factor and d minimum depth goal)
 breadthFirstSearch :: (Eq s) => (s -> Bool)       -- ^ function used to check if the goal is reached
                              -> (s -> [(s, Int)]) -- ^ generator of states
                              -> s                 -- ^ initial state
                              -> [s]               -- ^ returns list of states
 breadthFirstSearch = search P.breadthFirstPolicy Generation
 
--- | 'uniform-cost search' implementation
+-- | generalization of @breadthFirstSearch@ when paths costs are not all equals on the same level
 uniformCostSearch :: (Eq s) => (s -> Bool)       -- ^ function used to check if the goal is reached
                             -> (s -> [(s, Int)]) -- ^ generator of states
                             -> s                 -- ^ initial state
                             -> [s]               -- ^ returns list of states
 uniformCostSearch = search P.uniformCostPolicy Expansion
 
--- | 'depth-first search'  implementation
+-- | search with depth first policy. The deepest node is always expanded first, it is not /complete/ or
+--   /optimal/ but keeps only one route at a time in memory so it has a spatial complexity of /O(bm)/ with
+--   b branching factor and m max depth in the state space (can be infinite). If in the state graph there are
+--   loops then it will loop forever. Time complexity is /O(b^m)/.
 depthFirstSearch :: (Eq s) => (s -> Bool)       -- ^ function used to check if the goal is reached
                            -> (s -> [(s, Int)]) -- ^ generator of states
                            -> s                 -- ^ initial state
                            -> [s]               -- ^ returns list of states
 depthFirstSearch = search P.depthFirstPolicy Generation
 
--- | 'greedy best first search' implementation
+-- INFORMED SEARCH ALGORITHMS --
+
+-- | informed search algorithm which take into account only the value of the heuristic function in order to 
+--   choose next state, it is greedy cause it tries to reach the goal as fast as possible.It is not /optimal/
+--   and can not be /complete/, but really depends on the heuristic function.
 greedyBestFirstSearch :: (Eq s) => (s -> Int)        -- ^ heuristic cost function
                                 -> (s -> Bool)       -- ^ function used to check if the goal is reached
                                 -> (s -> [(s, Int)]) -- ^ generator of states
@@ -122,7 +136,8 @@ greedyBestFirstSearch :: (Eq s) => (s -> Int)        -- ^ heuristic cost functio
                                 -> [s]               -- ^ returns list of states
 greedyBestFirstSearch h = search (P.greedyBestFirstPolicy h) Generation
 
--- | 'A* search' implementation
+-- | A* search is a informed search algorithm which take into account both cost to reach a node and heuristic function
+--   in order to choose next state. It behaves as a @uniformCostSearch@ with a heuristic function
 aStarSearch :: (Eq s) => (s -> Int)        -- ^ heuristic cost function
                       -> (s -> Bool)       -- ^ function used to check if the goal is reached
                       -> (s -> [(s, Int)]) -- ^ generator of states
