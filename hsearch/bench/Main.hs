5200e9a9c39727b15c99d8ad231fb7d80405ea82
Andrea Conti <contiandrea96@gmail.com>
2019-03-25 14:11:08 +0100

    bench hsearch develop

--- hsearch/bench/Main.hs
+++ hsearch/bench/Main.hs
@@ -32,17 +32,8 @@ stateGenerator branching maxNum state  = let next   = increase state 1
                                                 then []
                                                 else zip (others ++ [next]) [1,1..]
 
-breadthFirstBench25  = breadthFirstSearch (isValue 5)  (stateGenerator 2 5)
-breadthFirstBench210 = breadthFirstSearch (isValue 10) (stateGenerator 2 10)
-breadthFirstBench215 = breadthFirstSearch (isValue 15) (stateGenerator 2 15)
-breadthFirstBench220 = breadthFirstSearch (isValue 20) (stateGenerator 2 20)
-breadthFirstBench45  = breadthFirstSearch (isValue 5)  (stateGenerator 4 5)
-breadthFirstBench410 = breadthFirstSearch (isValue 10) (stateGenerator 4 10)
-
-depthFirstBench5  = depthFirstSearch (isValue 5)  (stateGenerator 2 5)
-depthFirstBench10 = depthFirstSearch (isValue 10) (stateGenerator 2 10)
-depthFirstBench20 = depthFirstSearch (isValue 20) (stateGenerator 2 20)
-
+breadthFirstBench branching depth = breadthFirstSearch (isValue depth) (stateGenerator branching depth)
+depthFirstBench branching depth = depthFirstSearch (isValue depth) (stateGenerator branching depth) 
 
 -- command line output recap
 -- time    : OLS regression, estimates the time needed for a single execution
@@ -53,25 +44,31 @@ depthFirstBench20 = depthFirstSearch (isValue 20) (stateGenerator 2 20)
 -- std dev : standard deviation
 
 confs = defaultConfig {
-          reportFile  = Just $ "bench-results/report.html"
+          reportFile  = Just "bench-results/report.html"
         , regressions = [(["allocated", "numGcs"], "iters")]
     }
 
 main = defaultMainWith confs
     [ bgroup "breadth-first-search bench"
-        [ bench "B=2, D=5"  $ whnf breadthFirstBench25  start
---        , bench "B=2, D=10" $ whnf breadthFirstBench210 start
---        , bench "B=2, D=15" $ whnf breadthFirstBench215 start
---        , bench "B=2, D=20" $ whnf breadthFirstBench220 start
---        , bench "B=4, D=5"  $ whnf breadthFirstBench45  start
---        , bench "B=4, D=10" $ whnf breadthFirstBench410 start
-        -- !! Bench B=4 D=15 !! Out of memory (16Gb RAM)
+        [ bench "B=2, D=5"  $ whnf (breadthFirstBench 2 5)  start
+        , bench "B=2, D=10" $ whnf (breadthFirstBench 2 10) start
+        , bench "B=2, D=15" $ whnf (breadthFirstBench 2 15) start
+        , bench "B=2, D=20" $ whnf (breadthFirstBench 2 20) start
+        , bench "B=4, D=5"  $ whnf (breadthFirstBench 4 5)  start
+        , bench "B=4, D=10" $ whnf (breadthFirstBench 4 10) start
+        {- , bench "B=4, D=15" $ whnf (breadthFirstBench 4 15) start -}
+        {- , bench "B=4, D=20" $ whnf (breadthFirstBench 4 20) start -}
         ]
     
    , bgroup "depth-first-search bench"
-       [ bench "B=2, D=5"  $ whnf depthFirstBench5  start
---        , bench "B=2, D=10" $ whnf depthFirstBench10 start
---        , bench "B=2, D=20" $ whnf depthFirstBench20 start
+       [ bench "B=2, D=5"  $ whnf (depthFirstBench 2 5)  start
+       , bench "B=2, D=10" $ whnf (depthFirstBench 2 10) start
+       , bench "B=2, D=15" $ whnf (depthFirstBench 2 15) start
+       , bench "B=2, D=20" $ whnf (depthFirstBench 2 20) start
+       , bench "B=4, D=5"  $ whnf (depthFirstBench 4 5)  start
+       , bench "B=4, D=10" $ whnf (depthFirstBench 4 10) start
+       , bench "B=4, D=15" $ whnf (depthFirstBench 4 15) start
+       , bench "B=4, D=20" $ whnf (depthFirstBench 4 20) start
        ]
     
     ]
