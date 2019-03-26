850ca11168343ed4948be1bb67b2407e83732e26
Andrea Conti <contiandrea96@gmail.com>
2019-03-26 22:43:50 +0100

    PrioritySetFringe new implementation

--- hsearch/src/Data/AI/Search/Fringe/PrioritySetFringe.hs
+++ hsearch/src/Data/AI/Search/Fringe/PrioritySetFringe.hs
@@ -9,37 +9,37 @@ module Data.AI.Search.Fringe.PrioritySetFringe
 
 import qualified Data.PQueue.Prio.Min as PQ
 import           Data.PQueue.Prio.Min (MinPQueue)
+import qualified Data.Set as S
+import           Data.Set (Set)
 import           Data.AI.Search.Fringe
 import           Data.List
+import           Control.Monad
 
 -- | PSFringe implements a fringe which filters in @insert@ calls closed nodes all 
 --   items returned at least once by @next@ function
-data PSFringe i s = PSFringe !(PQ.MinPQueue Int s) -- ^ Set of closed nodes
+data PSFringe i s = PSFringe !(Set s)              -- ^ Set of closed nodes
                              !(PQ.MinPQueue i s)   -- ^ Priority Queue used
                              (s -> i)              -- ^ ordering function
 
-instance (Ord i, Eq s) => Fringe PSFringe i s where
+instance (Ord i, Ord s) => Fringe PSFringe i s where
     
     {-# INLINABLE next #-}
-    next (PSFringe closed queue ord) =
-        let minNode  = snd <$> PQ.getMin queue
-            newQueue = PQ.deleteMin queue
-        in case minNode of
-            Nothing -> Nothing
-            Just n  -> return $! (PSFringe (PQ.insert 0 n closed) newQueue ord, n)
+    next (PSFringe closed queue ord) = do
+        minNode <- snd <$> PQ.getMin queue
+        return $! (PSFringe (S.insert minNode closed) (PQ.deleteMin queue) ord, minNode)
 
     {-# INLINABLE insert #-}
     insert (PSFringe cl q ord) ns =
-        let valids  = (nub ns) \\ PQ.elemsU cl
+        let valids  = filter (\x -> S.notMember x cl) ns 
             keys     = map ord valids
             newQueue = PQ.fromList (zip keys valids) `PQ.union` q
         in  PSFringe cl newQueue ord
 
 instance (Show s, Ord i) => Show (PSFringe i s) where
-    show (PSFringe closed queue _) = "Closed: "    ++ (show . map snd . PQ.toAscList $ closed)
-                                ++ " | Fringe: " ++ (show . map snd . PQ.toAscList $ queue)
+    show (PSFringe closed queue _) = "Closed: "    ++ (show closed)
+                                  ++ " | Fringe: " ++ (show . map snd . PQ.toAscList $ queue)
 
 
 -- | build a \PSFringe\ to use
 empty :: (s -> i) -> PSFringe i s
-empty = PSFringe PQ.empty PQ.empty
+empty = PSFringe S.empty PQ.empty
