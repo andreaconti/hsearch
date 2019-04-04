a209be187bbfac2b55ce62fb10e1be6b6746cf21
Andrea Conti <contiandrea96@gmail.com>
2019-04-04 08:06:45 +0200

    bounded utility function

--- hsearch/src/AI/Games/ZeroSum/Internal.hs
+++ hsearch/src/AI/Games/ZeroSum/Internal.hs
@@ -41,10 +41,10 @@ type MaxDepth = Int
 
 -- UTILS --
 
-data Funcs s a = Funcs
-                   { utility      :: (s -> Double)
-                   , terminalTest :: (GNode s a -> Bool)
-                   , generator    :: (s -> [(a, s)])
+data Funcs s a d = Funcs
+                   { utility      :: s -> d
+                   , terminalTest :: GNode s a -> Bool
+                   , generator    :: s -> [(a, s)]
                    }
 
 {-# INLINE headMay #-}
@@ -55,58 +55,61 @@ headMay (GNode{..}:_) = Just action
 -- FUNCTIONS --
 
 {-# INLINE minimax #-}
-minimax :: MaxDepth              -- ^ Int: depth until search
-        -> (s -> Double)         -- ^ utility function : output must be between [0,1]
+minimax :: (Ord d, Bounded d)
+        => MaxDepth              -- ^ Int: depth until search
+        -> (s -> d)              -- ^ utility function : high values for Max, low values for Min
         -> (s -> Bool)           -- ^ in order to recognize game terminal states
         -> (s -> [(a, s)])       -- ^ generator of states and actions
         -> s                     -- ^ current state
         -> Maybe a               -- ^ return: best action
 minimax m utility terminalTest generator state = do
     guard  $ m > 0
-    let terminalTest' = (\(GNode{..}) -> terminalTest state || depth < m)
+    let terminalTest' GNode{..} = terminalTest state || depth < m
         fs = Funcs utility terminalTest' generator
-    headMay . map fst . sortBy (comparing $ negate . snd) $ map (minValue (-1) 1 fs) (map (fromTuple 0) (generator state))
+    headMay . map fst . sortBy (flip compare `on` snd) $ map (minValue minBound maxBound fs . fromTuple 0) (generator state)
 
 -- MIN NODES --
 
 {-# INLINE minValue #-}
-minValue :: Alpha                 -- ^ best value found for max
-         -> Beta                  -- ^ best value found for min
-         -> Funcs s a             -- ^ funcs
+minValue :: (Ord d, Bounded d) 
+         => d                     -- ^ best value found for max
+         -> d                     -- ^ best value found for min
+         -> Funcs s a d           -- ^ funcs
          -> GNode s a             -- ^ node
-         -> (GNode s a, Utility)
+         -> (GNode s a, d)
 minValue alpha beta fs@Funcs{..} node@GNode{..}
     | terminalTest node = (node, utility state)
-    | otherwise         = findMin 1 alpha beta fs . map (fromTuple (depth+1)) $ generator state
+    | otherwise         = findMin maxBound alpha beta fs . map (fromTuple (depth+1)) $ generator state
 
 
 {-# INLINE findMin #-}
-findMin :: Utility -> Alpha -> Beta -> Funcs s a -> [GNode s a] -> (GNode s a, Utility)
+findMin :: (Bounded d, Ord d) => d -> d -> d -> Funcs s a d -> [GNode s a] -> (GNode s a, d)
 findMin v alpha beta fs@Funcs{..} (n:ns) =
     let (node, minV) = maxValue alpha beta fs n
         v' = max v minV
-    in case v' <= alpha of
-        True -> (node, v')
-        False -> findMin v' alpha (min beta v') fs ns
+    in if v' <= alpha
+        then (node, v')
+        else findMin v' alpha (min beta v') fs ns
 
 -- MAX NODES --
 
 {-# INLINE maxValue #-}
-maxValue :: Alpha                 -- ^ best value found for max
-         -> Beta                  -- ^ best value found for min
-         -> Funcs s a             -- ^ funzioni
+maxValue :: (Bounded d, Ord d)
+         => d                     -- ^ best value found for max
+         -> d                     -- ^ best value found for min
+         -> Funcs s a d           -- ^ funzioni
          -> GNode s a             -- ^ node
-         -> (GNode s a, Utility)
+         -> (GNode s a, d)
 maxValue alpha beta fs@Funcs{..} node@GNode{..}
     | terminalTest node = (node, utility state)
-    | otherwise         = findMax (-1) alpha beta fs . map (fromTuple (depth+1)) $ generator state
+    | otherwise         = findMax minBound alpha beta fs . map (fromTuple (depth+1)) $ generator state
     
 
 {-# INLINE findMax #-}
-findMax :: Utility -> Alpha -> Beta -> Funcs s a -> [GNode s a] -> (GNode s a, Utility)
+findMax :: (Bounded d, Ord d) => d -> d -> d -> Funcs s a d -> [GNode s a] -> (GNode s a, d)
 findMax v alpha beta fs@Funcs{..} (n:ns) =
     let (node, minV) = minValue alpha beta fs n 
         v' = max v minV
-    in case v' >= beta of
-        True  -> (node, v')
-        False -> findMax v' (max alpha v') beta fs ns
+    in if v' >= beta
+        then (node, v')
+        else findMax v' (max alpha v') beta fs ns
