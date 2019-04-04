cb051cd3f04eed194a6624324200a054cddf69a6
Andrea Conti <contiandrea96@gmail.com>
2019-04-04 18:18:46 +0200

    tic-tac-toe game completed

--- hsearch-examples/src/AI/Search/Examples/TicTacToe.hs
+++ hsearch-examples/src/AI/Search/Examples/TicTacToe.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiWayIf #-}
 module AI.Search.Examples.TicTacToe where
 
 --------------------------------------
@@ -15,31 +16,32 @@ import           Data.Maybe (catMaybes, mapMaybe, fromJust)
 
 import           AI.Games.ZeroSum (minimax)
 
+import           System.Console.Haskeline
+
 -- Types --
 
-data TicTac = X | O | Empty
+data TicTac = X | O | E
     deriving Eq
 
 instance Show TicTac where
     show X = "X"
     show O = "O"
-    show Empty = " "
+    show E = " "
 
 type Table = Matrix TicTac
 
-emptyTable = M.fromList 3 3 (replicate 9 Empty)
+emptyTable = M.fromList 3 3 (replicate 9 E)
 
 -- utils --
 
-getAllTriple t = V.map (\x -> M.getCol x $ t <|> M.transpose t) (V.iterateN 6 (+1) 1)
+getAllSequences t = V.map (\x -> M.getCol x $ t <|> M.transpose t) (V.iterateN 6 (+1) 1) `V.snoc` M.getDiag t `V.snoc` V.fromList [t ! (1,3), t ! (2,2), t ! (3,1)]
 
-allX  triple = (==3) $ V.length . V.filter (==X) $ triple
-findX triple = not $ V.null . V.filter (==X) $ triple
-allO  triple = (==3) $ V.length . V.filter (==O) $ triple
-findO triple = not $ V.null . V.filter (==O) $ triple
-findE triple = not $ V.null . V.filter (==Empty) $ triple
-onlyXE triple = findX triple && findE triple && not (findO triple)
-onlyOE triple = findO triple && findE triple && not (findX triple)
+numIn :: Int -> Int -> Int -> Table -> Int
+numIn numX numO numE t = V.length . V.filter (found numX numO numE) $ getAllSequences t
+    where found x o e v = let x' = V.length . V.filter (==X) $ v
+                              o' = V.length . V.filter (==O) $ v
+                              e' = V.length . V.filter (==E) $ v
+                          in  x == x' && o == o' && e == e'
 
 -- Actions and functions --
 
@@ -47,40 +49,53 @@ type Action = Table -> Table
 
 putSign :: Table -> TicTac -> (Int, Int) -> Maybe (Action, Table)
 putSign t symbol pos = case t ! pos of
-    Empty -> Just (M.setElem symbol pos, M.setElem symbol pos t)
-    _     -> Nothing
+    E -> Just (M.setElem symbol pos, M.setElem symbol pos t)
+    _ -> Nothing
 
 
 generateStates :: Table -> [(Action, Table)]
 generateStates t = mapMaybe (putSign t X) [(x,y) | x <- [1..3], y <- [1..3] ]
 
 utility :: Table -> Int
-utility t = let triples = getAllTriple t
-                good    = V.length . V.filter onlyXE $ triples
-                bad     = V.length . V.filter onlyOE $ triples
-            in  good - bad
+utility t = let allx = numIn 3 0 0 t
+                allo = numIn 0 3 0 t
+                x2e1 = numIn 2 0 1 t
+                o2e1 = numIn 0 2 1 t
+                x1e2 = numIn 1 0 2 t
+                o1e2 = numIn 0 1 2 t
+            in if | emptyTable == t -> 0 
+                  | allx >= 1       -> 300
+                  | allo >= 1       -> (-300)
+                  | otherwise       -> x2e1 * 50 - o2e1 * 100 + x1e2 * 20 - o1e2 * 20
 
 endGame :: Table -> Bool
-endGame t = any (\x -> allX x || allO x) (getAllTriple t) 
+endGame t = numIn 3 0 0 t >= 1 || numIn 0 3 0 t >= 1 || notElem E (M.toList t)
 
 nextCross = minimax 30 utility endGame generateStates
 
 -- terminal game --
 
-meAndYou :: Table -> IO Table
-meAndYou t = do
+actionAI :: Table -> InputT IO Table
+actionAI t = do
+    outputStrLn $ show t
     let f  = fromJust . nextCross $ t
         t' = f t
+    return t'
 
-    print t'
-    putStr "Enter action: "
-    pos <- read <$> getLine :: IO (Int, Int) 
-    let t'' = snd . fromJust . putSign t' O $ pos 
-    if endGame t' || endGame t''
-        then do { print t'' ; putStrLn "END GAME"; return t'' }
-        else meAndYou t''
+actionPlayer :: Table -> InputT IO Table
+actionPlayer t = do
+    outputStrLn $ show t
+    pos <- read . fromJust <$> getInputLineWithInitial "Enter (Int, Int) >> " ("", "") :: InputT IO (Int, Int)
+    let t' = snd . fromJust . putSign t O $ pos
+    return t'
+
+game' :: Table -> [Table -> InputT IO Table] -> InputT IO ()
+game' t (p:ps) = do 
+    t' <- p t
+    if endGame t'
+        then outputStrLn "END GAME"
+        else game' t' (ps ++ [p])
 
 game :: IO ()
-game = do 
-    meAndYou emptyTable
-    return ()
+game = runInputT defaultSettings $ game' emptyTable [actionPlayer, actionAI]
+
